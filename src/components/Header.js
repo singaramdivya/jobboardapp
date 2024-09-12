42c7ff941fa10e82cede35129eac1ee6f93d681a
singaramdivya <123141756+singaramdivya@users.noreply.github.com>
2024-09-13 01:10:03 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,35 @@
-// src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 function Header() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
     <header>
-        <div className="navbar">
+      <div className="navbar">
         <div className="logo">Find JOB</div>
-        <ul className="menu">
-            <li><a href="/jobs">Browse Jobs</a></li>
-            <li><a href="#">Employers</a></li>
-            <li><a href="#">Jobseekers</a></li>
-            <li><a href="#">Career Advice</a></li>
-            <li><a href="#">Product</a></li>
-            <li><a href="#">Contact Us</a></li>
+
+        <ul className={`menu ${menuActive ? 'active' : 'hidden'}`}>
+          <li><a href="/jobs">Browse Jobs</a></li>
+          <li><a href="#">Employers</a></li>
+          <li><a href="#">Jobseekers</a></li>
+          <li><a href="#">Career Advice</a></li>
+          <li><a href="#">Product</a></li>
+          <li><a href="#">Contact Us</a></li>
         </ul>
+
         <div className="auth">
-            <li><a href="#">Sign In</a></li>
+          <li><a href="#">Sign In</a></li>
         </div>
-        </div>
+
+        <span className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </span>
+      </div>
     </header>
   );
 }
